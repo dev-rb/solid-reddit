40f85adbe7f99b1191693de7ac47bb28f3e76089
dev-rb <43100342+dev-rb@users.noreply.github.com>
2022-12-06 14:28:17 -0500

    Move icons to icons directory. Add post interactions. Add unstyled-button class.

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,6 +1,9 @@
 import { createTimeAgo } from '@solid-primitives/date';
 import { createSignal, Match, Show, Switch } from 'solid-js';
 import { Media, PostDetails } from '~/types/reddit';
+import { CommentIcon } from '../icons/CommentIcon';
+import { FallbackIcon } from '../icons/SubredditFallbackIcon';
+import { UpvoteIcon, DownvoteIcon } from '../icons/VoteIcons';
 import styles from './post.module.css';
 
 interface PostProps extends PostDetails {}
@@ -54,7 +57,9 @@ const Post = (props: PostProps) => {
 					<div class={styles.postMainContent}>
 						<Switch>
 							<Match when={postType() === 'text'}>
-								<div class={styles.textContent}> {props.selftext} </div>
+								<Show when={props.selftext.length}>
+									<div class={styles.textContent}> {props.selftext} </div>
+								</Show>
 							</Match>
 
 							<Match when={postType() === 'image'}>
@@ -79,58 +84,18 @@ const Post = (props: PostProps) => {
 						</Switch>
 					</div>
 				</div>
+				<div class={styles.postInteractions}>
+					<button class={`unstyled-button ${styles.postComments}`}>
+						<CommentIcon />
+						<span>{props.num_comments} Comments</span>
+					</button>
+				</div>
 			</div>
 		</div>
 	);
 };
 
 export default Post;
-
-const FallbackIcon = () => {
-	return (
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			viewBox="0 0 20 20"
-			class="fallback-subreddit-icon"
-		>
-			<path d="M16.5,2.924,11.264,15.551H9.91L15.461,2.139h.074a9.721,9.721,0,1,0,.967.785ZM8.475,8.435a1.635,1.635,0,0,0-.233.868v4.2H6.629V6.2H8.174v.93h.041a2.927,2.927,0,0,1,1.008-.745,3.384,3.384,0,0,1,1.453-.294,3.244,3.244,0,0,1,.7.068,1.931,1.931,0,0,1,.458.151l-.656,1.558a2.174,2.174,0,0,0-1.067-.246,2.159,2.159,0,0,0-.981.215A1.59,1.59,0,0,0,8.475,8.435Z"></path>
-		</svg>
-	);
-};
-
-const UpvoteIcon = () => {
-	return (
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="1em"
-			height="1em"
-			preserveAspectRatio="xMidYMid meet"
-			viewBox="0 0 24 24"
-		>
-			<path
-				fill="currentColor"
-				d="M12.781 2.375c-.381-.475-1.181-.475-1.562 0l-8 10A1.001 1.001 0 0 0 4 14h4v7a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-7h4a1.001 1.001 0 0 0 .781-1.625l-8-10zM15 12h-1v8h-4v-8H6.081L12 4.601L17.919 12H15z"
-			/>
-		</svg>
-	);
-};
-
-const DownvoteIcon = () => {
-	return (
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="1em"
-			height="1em"
-			preserveAspectRatio="xMidYMid meet"
-			viewBox="0 0 24 24"
-		>
-			<path
-				fill="currentColor"
-				d="M20.901 10.566A1.001 1.001 0 0 0 20 10h-4V3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v7H4a1.001 1.001 0 0 0-.781 1.625l8 10a1 1 0 0 0 1.562 0l8-10c.24-.301.286-.712.12-1.059zM12 19.399L6.081 12H10V4h4v8h3.919L12 19.399z"
-			/>
-		</svg>
-	);
-};
 
 const ImageContent = (props: PostDetails) => {
 	const image =
---
+++ src/components/icons/CommentIcon.tsx
@@ -0,0 +1,16 @@
+export const CommentIcon = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="1em"
+			height="1em"
+			preserveAspectRatio="xMidYMid meet"
+			viewBox="0 0 24 24"
+		>
+			<path
+				fill="currentColor"
+				d="m22 22l-4-4H4q-.825 0-1.412-.587Q2 16.825 2 16V4q0-.825.588-1.413Q3.175 2 4 2h16q.825 0 1.413.587Q22 3.175 22 4ZM4 4v12h14.825L20 17.175V4H4Zm0 0v13.175V4Z"
+			/>
+		</svg>
+	);
+};
